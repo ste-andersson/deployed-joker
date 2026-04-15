6d2ff31532f74b055c8e452dc00085d6967b25b1
Stefan Andersson <stefan.andersson@sveki.se>
2026-04-15 16:53:56 +0200

    Edit vite.config.ts
    Co-authored-by: Sebastian Thunberg <sebastian.thunberg@hotmail.se>
    Co-authored-by: Venujan Nagendirakumar <venujan.nagendirakumar@appliedtechnology.se>
    Co-authored-by: Max Hellenberg <max.hellenbrg@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/deployed-joker/",
   test: {
